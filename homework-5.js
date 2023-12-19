36245505f1cd9ac8ce533b1241907b848c5022c0
Adham Ismonov <kkirapm@gmail.com>
2023-12-19 14:49:02 +0500

    3

---
+++ homework-5.js
@@ -0,0 +1,45 @@
+// 1
+
+// function task1(n) {
+//     if (n == 1) {
+//       return 1;
+//     } 
+//       return n * task1(n - 1);
+//   }
+// console.log(task1(5));
+
+// 2
+
+// function task2(n) {
+//   if (n == 0) {
+//     return 0;
+//   }
+//   return n + task2(n - 1);
+// }
+// console.log(task2(10));
+
+// 3
+
+// function task3(a, b) {
+//     if (b == 0) {
+//       return 1;
+//     } 
+//       return a * task3(a, b - 1); 
+//   }  
+// console.log(task3(3,3));
+
+// 4
+
+// function task4() {
+//   let cnt = 0;
+//   return () => {
+//     return ++cnt;
+//   };
+// }
+// let first = task4 ();
+// console.log(first());
+// console.log(first());
+// console.log(first());
+
+
+
